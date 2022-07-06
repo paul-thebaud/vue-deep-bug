11279a9d2fdab7e0b4dc48918b26ed5b86e51508
Paul Thebaud <paul.thebaud29@gmail.com>
2022-07-06 20:09:03 +0200

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+    <AppChild />
+</template>
+
+<script>
+import AppChild from './AppChild.vue';
+
+export default {
+    name: 'App',
+    components: { AppChild },
+};
+</script>
+
+<style scoped>
+:deep(.app-child__hello) {
+    color: red;
+}
+</style>
---
+++ src/AppChild.vue
@@ -0,0 +1,11 @@
+<template>
+    <div>
+        <p class="app-child__hello">Hello World!</p>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'AppChild',
+};
+</script>
